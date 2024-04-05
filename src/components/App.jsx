da546ba0239792c2c896590e12f0af6a4bf45c80
ramocoblisan <ramonaioana67@yahoo.com>
2024-04-05 22:36:22 +0300

    App.jsx updated

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,9 @@
 import Profile from "./Profile/Profile";
 import user from "./data/user.json";
 import data from "./data/data.json";
-import Statistics from "./Statistics/Statistics"
-import friends from "./data/friends.json"
-import FriendList from "./FriendList/FriendList"
+import Statistics from "./Statistics/Statistics";
+import friends from "./data/friends.json";
+import FriendList from "./FriendList/FriendList";
 
 
 export const App = () => {
@@ -20,7 +20,7 @@ export const App = () => {
 
     <Statistics title="Upload stats" stats={data} />
     <Statistics stats={data} />
-    <FriendList friends={friends} />;
+    <FriendList friends={friends} />
    </>
   );
 };
